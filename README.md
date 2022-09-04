02f0ac04f528c636d327a1783795ebb3200b30c6
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-09-04 17:01:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Usage:
+
+* Edit config/config.json to your needs
+* Run python3 downloader.py to download the files
+
+### requirements:
+* python3
+* aria2c
---
+++ downloader.py
@@ -0,0 +1,75 @@
+import json
+import os
+import subprocess
+
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+
+urllib3.disable_warnings()
+
+
+def get_urls(page):
+    urls = []
+    req = requests.get(page, verify=False)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    print('Parsing iframes...')
+    for iframe in soup.find_all('iframe'):
+        r = requests.get(iframe.get('src'), verify=False)
+        iframe_soup = BeautifulSoup(r.text, 'html.parser')
+        for element in iframe_soup.find_all('source'):
+            urls.append(element.get('src'))
+
+    print(f'Found {len(urls)} urls to download on page "{page}".')
+    return urls
+
+
+def download(book_dir_path, url_list):
+    cmd = ['aria2c', '-i', f'{url_list}', '-d', f'{book_dir_path}']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    proc.wait()
+    output, errors = proc.communicate()
+    print(output)
+    if errors:
+        print(errors)
+
+
+def main():
+    # load config
+    with open(os.path.realpath(os.path.join('config', 'config.json')), 'r') as f:
+        config = json.loads(f.read())
+
+    # iterate through subjects
+    for subject, subject_data in config.get('subjects', {}).items():
+        print(f'Working on subject "{subject}"...')
+
+        # iterate through books
+        for book, page_url in subject_data.items():
+            print(f'Working on "{book}"...')
+
+            book_urls = get_urls(page_url)
+            book_dir_path = os.path.realpath(os.path.join(config.get('output', ''), subject, book))
+            url_list_path = os.path.realpath(os.path.join(book_dir_path, 'url_list.txt'))
+
+            try:
+                os.makedirs(book_dir_path)
+                print(f'Directory "{book_dir_path}" created.')
+            except FileExistsError:
+                print(f'Directory "{book_dir_path}" already exists.')
+            except Exception as e:
+                print(f'Error creating "{book_dir_path}": {e}')
+
+            # create an url list for this book
+            with open(url_list_path, 'w') as f:
+                f.write("\n".join(book_urls))
+
+            print(f'File list saved to "{url_list_path}".')
+            print(f'Downloading with aria2c...')
+
+            # feed the url list and the book directory to aria2c for download
+            download(book_dir_path, url_list_path)
+
+
+if __name__ == '__main__':
+    main()
